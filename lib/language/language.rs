cf263bbca3c8d903f7f55a0774f7ac202bea9fe6
Max Brunsfeld <maxbrunsfeld@gmail.com>
2024-02-26 12:52:52 -0800

    Fix formatting of safety doc for LanguageFn::from_raw
    Co-authored-by: Marshall <marshall@zed.dev>

--- lib/language/language.rs
+++ lib/language/language.rs
@@ -5,7 +5,9 @@ pub struct LanguageFn(unsafe extern "C" fn() -> *const ());
 impl LanguageFn {
     /// Creates a `LanguageFn`.
     ///
-    /// Safety: Only call this with language functions generated from grammars
+    /// # Safety
+    ///
+    /// Only call this with language functions generated from grammars
     /// by the Tree-sitter CLI.
     pub const unsafe fn from_raw(f: unsafe extern "C" fn() -> *const ()) -> Self {
         Self(f)
